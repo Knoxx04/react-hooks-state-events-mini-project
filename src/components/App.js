091bc73b019ba3ee1975c7a3b99c384d7b53ae32
Knoxx04 <onyiknoxx3@gmail.com>
2023-01-21 21:50:34 +0300

    lab done

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,39 @@
 import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import TaskList from "./TaskList";
+import NewTaskForm from "./NewTaskForm";
+import { useState } from "react";
+import CategoryFilter from "./CategoryFilter";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+	const [tasks, setTasks] = useState(TASKS);
+
+	
+	function filterTasks(CATEGORIES) {
+		if (CATEGORIES === "All") {
+			setTasks(TASKS);
+		} else {
+			let updList = TASKS.filter((letT) => letT.category === CATEGORIES);
+			setTasks(updList);
+		}
+	};
+	function onTaskFormSubmit(updList) {
+		setTasks([...tasks, updList]);
+	};
+
+	function delTasks(task) {
+		let updList = tasks.filter((newT) => newT.text !== task);
+		setTasks(updList);
+	};
+
+	return (
+		<div className="App">
+			<h2>My tasks</h2>
+			<CategoryFilter categories={CATEGORIES} filterTasks={filterTasks}/>
+			<NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+			<TaskList tasks={tasks} delTasks={delTasks}/>
+		</div>
+	);
 }
 
 export default App;
